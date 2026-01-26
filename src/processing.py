a033bc8fe1e51c9f8643f84e1aca8e819dc7d036
Sergey Shinkevich <sergey.shinkevich@hotmail.com>
2026-01-27 01:02:43 +0700

    Revert "Add function filter_by_state"
    This reverts commit 353e63e5c97fe72df548d74f5e06da5f6e538ea7.

--- src/processing.py
+++ src/processing.py
@@ -1,16 +1,11 @@
-def filter_by_state(list_dict: list[dict], state: str = "EXECUTED") -> list[dict]:
+def filter_by_state(list_dict: list[dict], state: str ='EXECUTED' ) -> list[dict]:
     """
     Функция возвращает новый список словарей, содержащий только те словари,
     у которых ключ state  соответствует указанному значению.
     """
     result: list[dict] = []
     for item in list_dict:
-        if item.get("state") == state:
+        if item.get('state') == state:
             result.append(item)
     return result
 
-
-def sort_by_date(list_dict: list[dict], up_down: bool = True) -> list[dict]:
-    """Функцию сортировки списка словарей по дате. Для направления сортировки - параметр up_down"""
-    result = sorted(list_dict, key=lambda list_dict: list_dict["date"], reverse=up_down)
-    return result

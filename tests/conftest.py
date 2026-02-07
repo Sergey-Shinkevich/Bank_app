905b176e031f31e1c528a64d88394005c4573cc8
Sergey Shinkevich <sergey.shinkevich@hotmail.com>
2026-02-07 19:33:24 +0700

    Add tests and update  transaction_descriptions()

--- tests/conftest.py
+++ tests/conftest.py
@@ -72,7 +72,7 @@ def abnormal_list_dict_currency() -> list[dict]:
             "id": 939719570,
             "state": "EXECUTED",
             "date": "2018-06-30T02:08:58.425572",
-            "description": "Перевод организации",
+            "description": "Перевод c организации",
             "from": "Счет 75106830613657916952",
             "to": "Счет 11776614605963066702"
         },
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,10 +1,13 @@
+from collections.abc import generator
+
 import pytest
-from src.generators import filter_by_currency
+from src.generators import filter_by_currency, transaction_descriptions
 
 #Тесты функции filter_by_currency
 def test_filter_by_currency_empty() -> None:
     """Тестирование на пустой список"""
-    next(filter_by_currency([])) == "Пустой список"
+    generator = filter_by_currency([])
+    assert next(generator) == "Пустой список"
 
 def test_filter_by_currency_abnormal_data(abnormal_list_dict_currency) -> None:
     """Тестирование функции на неправильные данные"""
@@ -18,4 +21,23 @@ def test_filter_by_currency_normal_data(normal_list_dict_currency) -> None:
     assert next(generator) == normal_list_dict_currency[1]
     assert next(generator) == normal_list_dict_currency[3]
 
-#
+#Тесты функции transaction_descriptions()
+def test_transaction_descriptions_empty() -> None:
+    """Тестирование на пустой список"""
+    generator = transaction_descriptions([])
+    assert next(generator) == "Пустой список"
+
+def test_transaction_descriptions_normal_data(normal_list_dict_currency) -> None:
+    """Тестирование функции на правильные данные"""
+    generator = transaction_descriptions(normal_list_dict_currency)
+    assert next(generator) == "Перевод организации"
+    assert next(generator) == "Перевод со счета на счет"
+    assert next(generator) == "Перевод со счета на счет"
+    assert next(generator) == "Перевод с карты на карту"
+    assert next(generator) == "Перевод организации"
+
+def test_transaction_descriptions_abnormal_data(abnormal_list_dict_currency) -> None:
+    """Тестирование функции на неправильные данные"""
+    generator = transaction_descriptions(abnormal_list_dict_currency)
+    assert next(generator) == "Не корректное наименование операции"
+    assert next(generator) == "Перевод со счета на счет"

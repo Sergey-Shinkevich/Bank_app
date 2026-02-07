048a9b4319fb75cfc09285f2add9680c648d0476
Sergey Shinkevich <sergey.shinkevich@hotmail.com>
2026-02-07 18:49:21 +0700

    Add function transaction_descriptions()

--- src/generators.py
+++ src/generators.py
@@ -12,3 +12,13 @@ def filter_by_currency(trans: list[dict], curr: str="USD") -> Iterator:
             if item["operationAmount"]["currency"]["name"] == curr:
                 yield item
 
+def transaction_descriptions(trans: list[dict]) -> str:
+    if len(trans) == 0:
+        yield "Пустой список"
+    else:
+        correct_strings = {"Перевод организации", "Перевод со счета на счет", "Перевод с карты на карту"}
+        for item in trans:
+            if item["description"] in correct_strings:
+                yield item["description"]
+            else:
+                yield "Не корректное наименование операции"

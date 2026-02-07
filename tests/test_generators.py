3753bb30c9bd47a115ffa17fe1649d44720de5fc
Sergey Shinkevich <sergey.shinkevich@hotmail.com>
2026-02-07 22:52:57 +0700

    tests for card_number_generator

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,43 +1,66 @@
-from collections.abc import generator
-
 import pytest
-from src.generators import filter_by_currency, transaction_descriptions
+from mypy.binder import Iterator
 
-#Тесты функции filter_by_currency
+from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
+
+
+# Тесты функции filter_by_currency
 def test_filter_by_currency_empty() -> None:
     """Тестирование на пустой список"""
-    generator = filter_by_currency([])
+    generator: Iterator = filter_by_currency([])
     assert next(generator) == "Пустой список"
 
-def test_filter_by_currency_abnormal_data(abnormal_list_dict_currency) -> None:
+
+def test_filter_by_currency_abnormal_data(abnormal_list_dict_currency: list[dict]) -> None:
     """Тестирование функции на неправильные данные"""
     with pytest.raises(KeyError):
         next(filter_by_currency(abnormal_list_dict_currency))
 
-def test_filter_by_currency_normal_data(normal_list_dict_currency) -> None:
+
+def test_filter_by_currency_normal_data(normal_list_dict_currency: list[dict]) -> None:
     """Тестирование функции на правильные данные"""
-    generator = filter_by_currency(normal_list_dict_currency)
+    generator: Iterator = filter_by_currency(normal_list_dict_currency)
     assert next(generator) == normal_list_dict_currency[0]
     assert next(generator) == normal_list_dict_currency[1]
     assert next(generator) == normal_list_dict_currency[3]
 
-#Тесты функции transaction_descriptions()
+
+# Тесты функции transaction_descriptions()
 def test_transaction_descriptions_empty() -> None:
     """Тестирование на пустой список"""
-    generator = transaction_descriptions([])
+    generator: Iterator = transaction_descriptions([])
     assert next(generator) == "Пустой список"
 
-def test_transaction_descriptions_normal_data(normal_list_dict_currency) -> None:
+
+def test_transaction_descriptions_normal_data(normal_list_dict_currency: list[dict]) -> None:
     """Тестирование функции на правильные данные"""
-    generator = transaction_descriptions(normal_list_dict_currency)
+    generator: Iterator = transaction_descriptions(normal_list_dict_currency)
     assert next(generator) == "Перевод организации"
     assert next(generator) == "Перевод со счета на счет"
     assert next(generator) == "Перевод со счета на счет"
     assert next(generator) == "Перевод с карты на карту"
     assert next(generator) == "Перевод организации"
 
-def test_transaction_descriptions_abnormal_data(abnormal_list_dict_currency) -> None:
+
+def test_transaction_descriptions_abnormal_data(abnormal_list_dict_currency: list[dict]) -> None:
     """Тестирование функции на неправильные данные"""
-    generator = transaction_descriptions(abnormal_list_dict_currency)
+    generator: Iterator = transaction_descriptions(abnormal_list_dict_currency)
     assert next(generator) == "Не корректное наименование операции"
     assert next(generator) == "Перевод со счета на счет"
+
+
+# Тесты функции card_number_generator
+@pytest.mark.parametrize(
+    "digit, result",
+    [
+        (1, "0000 0000 0000 0001"),
+        (2, "0000 0000 0000 0002"),
+        (3, "0000 0000 0000 0003"),
+        (4, "0000 0000 0000 0004"),
+        (5, "0000 0000 0000 0005"),
+    ],
+)
+def test_card_number_generator(digit: int, result: str) -> None:
+    """Тест генератора карт"""
+    generator_card: Iterator = card_number_generator(digit, digit)
+    assert next(generator_card) == result

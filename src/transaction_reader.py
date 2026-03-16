cf09a6ff16f288003bd4a5f2e1fc75d6daf68232
Sergey Shinkevich <sergey.shinkevich@hotmail.com>
2026-03-17 04:41:09 +0700

    add function excel_read_to_dict

--- src/transaction_reader.py
+++ src/transaction_reader.py
@@ -1,6 +1,10 @@
 import csv
 
-def csv_read_to_dict(path):
+import pandas as pd
+
+
+def csv_read_to_dict(path: str) -> list[dict]:
+    """Функция читает CSV-файл и возвращает список словарей"""
     try:
         with open(path, "r", encoding="utf-8") as f:
             reader = csv.DictReader(f, delimiter=";")
@@ -9,9 +13,11 @@ def csv_read_to_dict(path):
         return []
 
 
-
-
-
-
-#a = csv_read_to_dict('../data/transactions.csv')
-#print(a)
+def excel_read_to_dict(path: str) -> list[dict]:
+    """Функция читает Excel-файл и возвращает список словарей"""
+    try:
+        excel_data = pd.read_excel(path)
+        result = excel_data.to_dict(orient="records")
+        return result
+    except Exception:
+        return []
